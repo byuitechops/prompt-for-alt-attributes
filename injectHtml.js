4707c087b4485b7e2b06fd700f02de05db0660bb
Ashley Stewart <ashleystewart8888@yahoo.com>
2018-03-29 15:44:55 -0600

    minor change

--- injectHtml.js
+++ injectHtml.js
@@ -1,8 +1,9 @@
 const pathLib = require('path');
 module.exports = (filename, image, inputId) => {
-    var imageName = pathLib.basename(image);
     return `<h2>File: ${filename}</h2>
-    <h3>Image: ${imageName}</h3>
-    <img src="${image}" id ='${inputId}' class="imageDiv">
-    <p>Alt: </p><input type ="text" id='${inputId}'>`;
+    <h3>Image: ${pathLib.basename(image)}</h3>
+    <div class="imageDiv">
+    <img src="${image}" id ='${inputId}'>
+    </div>
+    <input type ="text" class = 'altInput' id='${inputId}' placeholder =' Input Alt Text Here'>`;
 };
