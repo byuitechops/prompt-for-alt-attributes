6dc2618ff1c2599abecbf6c6eccd9d79634bb200
Ashley Stewart <ashleystewart8888@yahoo.com>
2018-03-29 15:45:06 -0600

    separate files

--- make-stuff-happen/injectContents.js
+++ make-stuff-happen/injectContents.js
@@ -1,42 +1,31 @@
 //2
 /*eslint no-unused-vars:1 */
+/*eslint no-debugger:1 */
 const injectHtml = require('../injectHtml.js'),
     changeAlts = require('./changeAlts.js'),
-    pathLib = require('path');
+    injectBrokenImgHtml = require('./injectBrokenImgHtml.js');
 
-module.exports = function (err, path, pages, noAltImgs, imageIDs) {
+module.exports = function (err, path, pages, noAltImgs, brokenImages) {
     //2. After all images are retrieved, inject each image into the html page.
     noAltImgs.forEach(function (image) {
-        // image.source = pathLib.resolve(path, image.source.split('?')[0]);
         var document = window.document;
-        var div = document.getElementById('bodyDiv');
-        //inject image 
-        console.log('PASSED SOURCE ', image.source);
-        div.innerHTML = injectHtml(image.imageFile, image.source, image.imgId);
-        //this returns the index # 
-        var equalIds = imageIDs.find((i) => {
-            return image.imgId === i;
-        });
-        if (equalIds.typeOf == 'number') {
-            //getText(image);
-        } else {
-            //changeAlts(err, null, null);
+        //create a new div each time, and not in the same div
+        var div = document.createElement('div', 'class="bodyDiv"');
+        // var div = document.getElementById('bodyDiv');
+        if (typeof image.source === 'undefined') {
+
+            console.log('source undefined', image);
         }
+        div.innerHTML = injectHtml(image.imageFile, image.source, image.imgId);
+        document.body.appendChild(div);
+    });
+    //broken images is weird now. Non broken are showing up.
+    brokenImages.forEach(function (brokenImg) {
+        console.log('BROKEN IMAGE -', brokenImg);
+        var document = window.document;
+        var div = document.createElement('div');
+        div.innerHTML = injectBrokenImgHtml(brokenImg.imageFile, brokenImg.source);
+        document.body.appendChild(div);
     });
 
-    /*this should happen after the user does stuff. May need to move to a new function */
-    /*function getText(image) {
-        var imgId = image.imgId,
-            //grab the text from the input box based on what image it is
-            text = window.document.getElementById(imgId).innerHTML();
-        makeChanges(image, text);
-    }
-
-    function makeChanges(image, text) {
-        //add text as the new alt property
-        noAltImgs.forEach((image) => {
-            image.alt === text;
-        });
-    }
-    changeAlts(null, pages, noAltImgs);*/
 };
