7c34a81c449faf3096d47280cd21ce99fbd9c827
Ashley Stewart <ashleystewart8888@yahoo.com>
2018-03-30 13:28:55 -0600

    non functional

---
+++ make-stuff-happen/makeHtmlChanges.js
@@ -0,0 +1,32 @@
+/*eslint no-unused-vars:1 */
+var changeAlts = require('./make-stuff-happen/changeAlts.js');
+
+/*--------------------------------------
+NEXT PART HAPPENS AFTER USER SUBMITS
+--------------------------------------*/
+//how is noAltImgs gonna be defined if this happens after a click event?
+function makeHtmlChanges(err, pages, noAltsImgs) {
+    var inputs = document.querySelectorAll('input'),
+        textList = [];
+    inputs.forEach(function (text) {
+        if (text.value !== '') {
+            textList.push({
+                id: text.id,
+                text: text.value
+            });
+        }
+    });
+    //this function needs to be wrapped in a noAltImgs.forEach.
+    textList.forEach(function (text) {
+        //if text id = image id, then call makeChanges(image, text.text)
+        if (text.id) {
+            makeChanges( /*,*/ text.text);
+        }
+    });
+
+    function makeChanges(image, text) {
+        //add text as the new alt property
+        image.alt === text;
+    }
+    changeAlts(null, pages);
+}
