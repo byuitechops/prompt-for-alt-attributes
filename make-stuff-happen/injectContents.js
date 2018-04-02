bf2a15274c4840f8d9bf54be34da2b96d39810f5
Ashley Stewart <ashleystewart8888@yahoo.com>
2018-04-02 13:00:03 -0600

    took out testing if statements

--- make-stuff-happen/injectContents.js
+++ make-stuff-happen/injectContents.js
@@ -2,24 +2,19 @@
 const injectHtml = require('./injectHtml.js'),
     injectBrokenImgHtml = require('./injectBrokenImgHtml.js');
 
-module.exports = function (err, path, pages, noAltImgs, brokenImages) {
+module.exports = function (err, noAltImgs, brokenImages) {
     //2. After all images are retrieved, inject each image into the html page.
+    var document = window.document;
     noAltImgs.forEach(function (image) {
-        var document = window.document;
         //create a new div each time, and not in the same div
-        var div = document.createElement('div', 'class="bodyDiv"');
-        // var div = document.getElementById('bodyDiv');
-        if (typeof image.source === 'undefined') {
-
-            console.log('source undefined', image);
-        }
-        div.innerHTML = injectHtml(image.imageFile, image.source, image.imgId);
+        var div = document.createElement('div', 'class="broken"');
+        div.innerHTML = injectHtml(image.imageFile, image.source, image.id);
         document.body.appendChild(div);
     });
-    //broken images is weird now. Non broken are showing up.
+    //I don't know how to catch this from the browser
+    // console.log('# broken Images', brokenImages.length);
     brokenImages.forEach(function (brokenImg) {
         console.log('BROKEN IMAGE -', brokenImg.source);
-        var document = window.document;
         var div = document.createElement('div');
         div.innerHTML = injectBrokenImgHtml(brokenImg.imageFile, brokenImg.source);
         document.body.appendChild(div);
