d176f866a21c231fd2a7b229ade95b68f14ae793
Ashley Stewart <ashleystewart8888@yahoo.com>
2018-04-03 16:11:13 -0600

    added id iterator, changed async function

--- make-stuff-happen/input1.js
+++ make-stuff-happen/input1.js
@@ -6,6 +6,7 @@ const cheerio = require('cheerio'),
     pathLib = require('path'),
     chalk = require('chalk'),
     injectContents = require('./make-stuff-happen/injectContents.js');
+var htmlFiles;
 
 function isUrl(urlString) {
     return urlString.includes('http');
@@ -13,7 +14,6 @@ function isUrl(urlString) {
 
 function isValidPath(imgPath) {
     var isValid;
-    // imgPath = imgPath.replace(/%20/g, ' '); //regex /g thing.
     fs.access(imgPath, (err, isValid) => {
         if (err) {
             isValid = false;
@@ -23,26 +23,39 @@ function isValidPath(imgPath) {
         return isValid;
     });
 }
+var imgId = 0;
+
+function iterateId(n) {
+    imgId = imgId + n;
+    return imgId;
+}
 
 function getImagesToName() {
+    //cannot read path property of this?
     var currentPath = document.getElementById('uploadFile').files[0].path;
+    console.log('chosen path: ', currentPath);
     if (currentPath == undefined) {
-        console.log('INVALID path chosen by user.');
+        console.log('INVALID path chosen by user. Please try again.');
+    } else if (isValidPath(currentPath) !== true) {
+        console.log('INVALID path chosen by user. Please try again.');
     }
 
     //1A. (on installation/first click) retrieve all the html files from the files
     function getAllPages() {
-        var htmlFiles = fs.readdirSync(currentPath)
-            .filter(function (file) {
-                return file.includes('.html');
-            })
-            .map(function (file) {
-                var correctPath = pathLib.resolve(currentPath, file);
-                return {
-                    file: file,
-                    contents: fs.readFileSync(correctPath, 'utf8')
-                };
-            });
+        htmlFiles = fs.readdir(currentPath, function (err, files) {
+            if (err) {
+                console.log(err);
+            }
+            return files;
+        }).filter((file) => {
+            return file.includes('.html');
+        }).map((file) => {
+            var correctPath = pathLib.resolve(currentPath, file);
+            return {
+                file: file,
+                contents: fs.readFileSync(correctPath, 'utf8')
+            };
+        });
         pagesToImageObjs(null, htmlFiles);
     }
 
@@ -73,8 +86,6 @@ function getImagesToName() {
                     ext = pathLib.parse(src).ext,
                     newSrc;
                 //if it's not a valid path, then identify it as a broken image.
-                //FIND OUT WHY IT'S ALWAYS EVALUATING AS TRUE IN THE ELECTRON CONSOLE
-                console.log('src: ', src);
                 if (isValidPath(src) == false) {
                     var item = {
                         imageFile: file.file,
@@ -102,9 +113,11 @@ function getImagesToName() {
                         source = src;
                     }
                     //push each image obj to later match it with an imgID
+                    //no id...?
                     alts.noAltImgs.push({
                         source: source,
                         imageFile: file.file,
+                        id: iterateId(1)
                     });
                     file.images.push(image);
                 }
@@ -113,7 +126,7 @@ function getImagesToName() {
         }, alts);
         console.log(chalk.magenta(' # images to name:', alts.noAltImgs.length));
         //only gets passed once...
-        injectContents(null, currentPath, htmlFiles, alts.noAltImgs, alts.brokenImgs);
+        injectContents(null, htmlFiles, alts.noAltImgs, alts.brokenImgs);
     }
     getAllPages();
 }
