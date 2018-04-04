7f1c5adb588a08f391cb4ea8855c682c20be7cd1
Ashley Stewart <ashleystewart8888@yahoo.com>
2018-04-04 12:55:03 -0600

    fixed image name

--- make-stuff-happen/injectHtml.js
+++ make-stuff-happen/injectHtml.js
@@ -1,9 +1,9 @@
 //2.5
-// const pathLib = require('path');
-// ${pathLib.parse(image).base}
+const pathLib = require('path');
 module.exports = (filename, image, id) => {
+    var imageName = pathLib.basename(image);
     return `<h2>File: ${filename}</h2>
-    <h3>Image: </h3>
+    <h3>Image: ${imageName}</h3>
     <div class="imageDiv brokenImage">
     <img src="${image}" id ='${id}'>
     </div>
