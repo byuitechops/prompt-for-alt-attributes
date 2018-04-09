89fcd4de67543768f72355db840f1701c299af23
Ashley Stewart <ashleystewart8888@yahoo.com>
2018-04-09 14:59:06 -0600

    clean up loops, use cheerio parser

--- make-stuff-happen/changeAlts.js
+++ make-stuff-happen/changeAlts.js
@@ -1,42 +1,39 @@
 //4
-/*eslint no-undef:1 */
 /*eslint no-debugger:1 */
 const pathLib = require('path'),
-    chalk = require('chalk'),
     output = require('./output.js'),
-    cheerio = require('cheerio');
+    $ = require('cheerio');
 
-module.exports = function changeAlts(err, newPages) {
+module.exports = function changeAlts(err, pages) {
     if (err) {
         console.error(err);
         return;
     }
-    debugger;
-    newPages.forEach(function (page) {
-        cheerio.load(page.contents);
-        page.newImages.forEach(function (image) {
-            //for each new image, find its matching image on the page
-            //TRYING TO GET THIS IMAGES TO HTML THING MATCHED UP -- maybe use old "newAlt" file as a reference
-            page.images.each(function (old) {
-                debugger;
-                var imageName = pathLib.basename(old.source);
-                console.log('old source', imageName);
-                console.log('new source', image.name);
-                //match old image sources by the new ones.. by name
-                if (imageName === image.name) {
-                    console.log(chalk.bgGreen('MATCH ' + image.name));
-                    changeAlt(image, image.alt);
+    pages.map(function (page) {
+        //get an array of the CHEERIO IMG OBJS on the page
+        var htmlImages = page.cheerioImgs;
+        //for every new image, find the matching image in page.contents,
+        htmlImages.forEach(function (cheerioImg) {
+            cheerioImg = $(cheerioImg);
+            var imageName = pathLib.basename(cheerioImg.attr('src'));
+            page.newImages.forEach(function (newImg) {
+                if (newImg.name === imageName) {
+                    changeAlt(cheerioImg, newImg.alt);
                 }
             });
         });
+
+        return pages;
     });
-    //helper function to change the alt text on the actual page, not just on the object
+    // and add the alt attribute to the html page.
     function changeAlt(image, newAlt) {
         if (newAlt === ' ' || newAlt === '') {
             output('Empty text is not allowed.');
         }
         debugger;
         image.attr('alt', newAlt);
+        //can't get this function to change anything on the object!?!?
+        console.log('changed image: ', image);
     }
-    output(null, newPages);
+    output(null, pages);
 };
