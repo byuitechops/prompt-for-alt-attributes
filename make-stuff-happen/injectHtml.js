f2596e8711adff7a55feeaa89b64b638beda122e
Ashley Stewart <ashleystewart8888@yahoo.com>
2018-04-11 12:13:14 -0600

    made changes for broken image injection

--- make-stuff-happen/injectHtml.js
+++ make-stuff-happen/injectHtml.js
@@ -1,6 +1,7 @@
 //2.5
 const pathLib1 = require('path');
-module.exports = (filename, image, id) => {
+
+function regular(filename, image, id) {
     var imageName = pathLib1.basename(image);
     return `<h2>File: ${filename}</h2>
     <h3>Image: ${imageName}</h3>
@@ -8,4 +9,15 @@ module.exports = (filename, image, id) => {
     <img src="${image}" id ='${id}'>
     </div>
     <input type ="text" class = 'altInput' id='${id}' placeholder =' Input Alt Text Here'>`;
+}
+
+function broken(filename, image) {
+    var imageName = pathLib1.basename(image);
+    return `<h2 class = "brokenImage">File: ${filename}</h2>
+    <h3 class="broken">Broken Image: ${imageName}</h3>
+    <p class="broken">NOTE: This image source is invalid. Please use the file name and image name to fix the image in the course.</p>`;
+}
+module.exports = {
+    regularImage: regular,
+    brokenImage: broken
 };
