90eeccd1ca445f9a07bd4f2c2f59ee7921dda1cc
Ashley Stewart <ashleystewart8888@yahoo.com>
2018-04-11 12:13:30 -0600

    updated function calls

--- make-stuff-happen/injectContents.js
+++ make-stuff-happen/injectContents.js
@@ -1,6 +1,5 @@
 //2
-const injectHtml = require('./injectHtml.js'),
-    injectBrokenImgHtml = require('./injectBrokenImgHtml.js');
+const injectHtml = require('./injectHtml.js');
 
 module.exports = function (err, noAltImgs, brokenImages) {
     if (err) {
@@ -12,13 +11,13 @@ module.exports = function (err, noAltImgs, brokenImages) {
     noAltImgs.forEach(function (image) {
         //create a new div each time, and not in the same div
         var div = document.createElement('div');
-        div.innerHTML = injectHtml(image.imageFile, image.source, image.id);
+        div.innerHTML = injectHtml.regularImage(image.imageFile, image.source, image.id);
         document.body.appendChild(div);
     });
     //I don't know how to catch this from the browser
     brokenImages.forEach(function (brokenImg) {
         var div = document.createElement('div', 'class="broken"');
-        div.innerHTML = injectBrokenImgHtml(brokenImg.imageFile, brokenImg.source);
+        div.innerHTML = injectHtml.brokenImage(brokenImg.imageFile, brokenImg.source);
         document.body.appendChild(div);
     });
 };
